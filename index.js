74ce56e091b53ef59598a41603a1b6bf624338aa
Shedrack Sunday <Hannania002@gmail.com>
2022-09-05 22:15:20 +0100

    Add js file

---
+++ index.js
@@ -0,0 +1,31 @@
+const menuBtn = document.querySelector('.navbar');
+const headerNavMobile = document.querySelector('.header-nav-mobile');
+const btnCloseNav = document.querySelector('.btn-close-nav');
+const linkPortfolio = document.querySelector('.link-portfolio');
+const linkAbout = document.querySelector('.link-about');
+const linkContact = document.querySelector('.link-contact');
+
+navbar.addEventListener('click', () => {
+  navbar.classList.add('hidden');
+  headerNavMobile.classList.remove('hidden');
+});
+
+btnCloseNav.addEventListener('click', () => {
+  navbar.classList.remove('hidden');
+  headerNavMobile.classList.add('hidden');
+});
+
+linkPortfolio.addEventListener('click', () => {
+  headerNavMobile.classList.add('hidden');
+  navbar.classList.remove('hidden');
+});
+
+linkAbout.addEventListener('click', () => {
+  headerNavMobile.classList.add('hidden');
+  navbar.classList.remove('hidden');
+});
+
+linkContact.addEventListener('click', () => {
+  headerNavMobile.classList.add('hidden');
+  navbar.classList.remove('hidden');
+});
