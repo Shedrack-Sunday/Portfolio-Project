2ca8f86650184162dfd5e9562f8aae54e794cb5d
Shedrack Sunday <Hannania002@gmail.com>
2022-09-07 19:42:45 +0100

    Add changes to js file

--- index.js
+++ index.js
@@ -137,43 +137,51 @@ const dataCards = [
 ];
 
 const cardWorkDetailsMobile = (index) => `
-  <article class="card-work-details-mobile">
-          <button type="button" class="btn-close-details">
+  <div class="modal-wrapper">
+    <article class="modal">
+          <div class="close-area">
+            <button type="button">
               <img src="./images/Icon-Cancel.png" alt="cancel icon">
-          </button>
-          <div class="card-image-details-mobile">
-              <img src=${dataCards[index].url} alt="sample">
+            </button>
           </div>
-          <h2 class="card-title-mobile">
+          <div>
+              <img class="modal-image" src=${dataCards[index].url} alt="sample">
+          </div>
+          <div class="modal-content">
+            <div class="title-container">
+              <h2 class="modal-title">
               ${dataCards[index].title}
-          </h2>
-          <div class="card-tags-mobile">
-              <li class="tag-mobile">${dataCards[index].tecnologies.tech1}</li>
-              <li class="tag-mobile">${dataCards[index].tecnologies.tech2}</li>
-              <li class="tag-mobile">${dataCards[index].tecnologies.tech3}</li>
+              </h2>
+              <div class="card-tags-mobile">
+                <li class="tag-mobile">${dataCards[index].tecnologies.tech1}</li>
+                <li class="tag-mobile">${dataCards[index].tecnologies.tech2}</li>
+                <li class="tag-mobile">${dataCards[index].tecnologies.tech3}</li>
+              </div>
+            </div> 
+            <div class="modal-btn">
+              <a href=${dataCards[index].linkVersion} class="custom-btn">
+                See Live
+                <img src="./images/Icon-rel.png" alt="see live icon">
+              </a>
+              <a href=${dataCards[index].linkSource} class="custom-btn">
+                See Sources
+                <img src="./images/Icon-GitHub.png" alt="see github icon">
+              </a>
+            </div>
+            <p class="modal-desc">
+              ${dataCards[index].description}
+            </p>
           </div>
-          <p class="card-paragraph-mobile">
-            ${dataCards[index].description}
-          </p>
-          <div class="card-button-mobile  button">
-              
-                 <a href=${dataCards[index].linkVersion} class="btn-card-details-mobile">
-                  See Live
-                  <img src="./images/Icon-rel.png" alt="see live icon">
-                  </a>
-                  <a href=${dataCards[index].linkSource} class="btn-card-details-mobile button">
-                  See Sources
-                  <img src="./images/Icon-GitHub.png." alt="see github icon">
-                  </a>
-          </div>
+          
           <script src="index.js"></script>
-  </article>
+    </article>
+  </div>
   `;
 
 for (let i = 0; i < popuplist.length; i += 1) {
   popuplist[i].addEventListener('click', () => {
     document.body.innerHTML = cardWorkDetailsMobile(i);
-    const btnCloseDetailsMobile = document.querySelector('.btn-close-details');
+    const btnCloseDetailsMobile = document.querySelector('.close-area');
     btnCloseDetailsMobile.addEventListener('click', async () => {
       window.location.reload();
     });
